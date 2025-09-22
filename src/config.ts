ed7f98cc2f51535f3f7b373e42498be56f5dc2a4
Sushmitha Katika <katikasushmitha7228@gmail.com>
2025-09-22 22:43:26 +0530

    initial commit

---
+++ src/config.ts
@@ -0,0 +1,103 @@
+export const siteConfig = {
+  name: "Sushmitha Katika",
+  title: "Recent B.Tech IT graduate | Java Full Stack Developer | ML & Web Enthusiast",
+  description: "Portfolio website of Sushmitha Katika",
+  accentColor: "#1d4ed8",
+  social: {
+    email: "katikasushmitha7228@gmail.com",
+    linkedin: "https://www.linkedin.com/in/sushmitha-katika-1aa1b9262/",
+    github: "https://github.com/SushmithaKatika",
+  },
+  
+  aboutMe:
+    "Aspiring Software Developer with expertise in Java Full Stack and Web Technologies. Recent B.Tech graduate in Information Technology (2025) with strong foundations in Java Full Stack Development, Web Technologies, and Core Computer Science concepts. Seeking an entry-level software development role to apply skills, contribute to impactful projects, and grow in a dynamic work environment." ,
+  skills: ["C", "Java", "Python", "JavaScript", "TypeScript", "SQL",
+  "HTML", "CSS", "Bootstrap", "React", "Next.js", "Node.js",
+  "Spring Boot", "Hibernate",
+  "MySQL", "MongoDB", "REST API",
+  "Git", "Maven", "Postman", "Eclipse", "Jupyter Notebook", "Google Colab", "Linux",
+  "OS", "DBMS", "DSA", "Computer Networks",
+  "Machine Learning", "Data Science"],
+  projects: [
+    {
+      name: "TrackSure-Blockchain",
+      description:
+      "Developed a blockchain-based system ensuring product authenticity for consumers. Built a transparent and traceable supply chain, enabling stakeholders to monitor production, distribution, and authentication in real time.",
+      link: "https://aidevroundup.com/?ref=devportfolio",
+      skills: ["Solidity", "Truffle", "JavaScript"],
+    },
+    {
+      name: "OsteoPredict: Osteoporosis Risk Prediction",
+      description:
+        "Developed a web-based ML tool predicting osteoporosis risk using user health data. Implemented secure data storage for longitudinal healthcare tracking. Presented at 5th IEEE CONIT 2025 (Paper ID: 2109).",
+      link: "https://github.com/SushmithaKatika/osteoporosis-risk-prediction-",
+      skills: ["Python", "Machine Learning", "Web Development"],
+    },
+  ],
+  experience: [
+    {
+      company: "AICTE Edu-Skills",
+      title: "Data Science Intern",
+      dateRange: "2024",
+      bullets: [
+        "Hands-on experience with Python, Data Analysis, Machine Learning, and Visualization",
+        "Performed EDA, data cleaning, predictive modeling, and deployment on real-world datasets",
+        "Built an end-to-end ML project demonstrating skills in model building & evaluation",
+      ],
+    },
+    
+  ],
+  education: [
+    {
+      school: "Anurag University",
+      degree: "Bachelor of Technology in Information Technology",
+      dateRange: "2021 - 2025",
+       gpa: "8.68",
+      achievements: [
+        "GPA: 8.68"
+      ],
+
+    },
+    {
+      school: "Narayana Junior College",
+      degree: "12th / Intermediate - MPC",
+      dateRange: "2019 - 2021",
+      achievements: [
+        "Percentage: 92%",
+      ],
+    },
+  ],
+certificates: [
+  {
+    name: "Wipro TalentNext – Java Full Stack",
+    issuer: "Wipro",
+    date: "2024",
+    link: "https://drive.google.com/file/d/1Ox9CJ5cdBUPcDL84TKVgKKK8x0KWXLx7/view?usp=sharing" // optional link to certificate
+  },
+  {
+    name: "Microsoft Certified: Azure Fundamentals",
+    issuer: "Microsoft",
+    date: "2024",
+    link: "https://drive.google.com/file/d/17m3J4VZlhj3eoCqL34BR_DYjzPGEmaxR/view?usp=sharing"
+  },
+  {
+    name: "Data Science Master Internship – AICTE",
+    issuer: "AICTE Edu-Skills",
+    date: "2024",
+    link: "https://drive.google.com/file/d/1pPKDx8qalDxMeHvGFU59Q-0lcxqhahah/view?usp=sharing"
+  },
+  {
+    name: "SQL (Basic) – HackerRank",
+    issuer: "HackerRank",
+    date: "2023",
+    link: "https://drive.google.com/file/d/131-Dhn6-YzybfudriWMgopO8admnpif7/view?usp=sharing"
+  },
+  {
+    name: "Networking Essentials & CCNA",
+    issuer: "Cisco",
+    date: "2023",
+    link: "https://drive.google.com/file/d/18HRgqmRTWfK8YTIkUQzfl9fE8Jg_0g8f/view?usp=sharing"
+  },
+]
+
+};
